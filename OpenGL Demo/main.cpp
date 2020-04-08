9d501dafeb59b4365538e135c3182384399bd0f7
Chen Z <chenzhu@ucsb.edu>
2020-04-08 16:39:08 -0700

    Initial Commit

---
+++ OpenGL Demo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGL Demo
+//
+//  Created by Stone Zhu on 4/8/20.
+//  Copyright © 2020 Stone Zhu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
